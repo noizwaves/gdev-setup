fa4e1302075a0e1da71bdc95725f9e8a11fe1930
Adam Neumann <1007983+noizwaves@users.noreply.github.com>
2023-10-07 14:16:00 -0700

    Run basic fixes when a step fails

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # gdev-setup
 
 Easily set up bare metal local dev environments.
+
+## Configuration
+
+### Commands
+
+Shell commands that:
+- exit with `0` when they succeed
+- exit with anything else when they fail
+
+### Fixes
+
+All fix commands are run in order exactly one time. `gdev-setup` continues regardless of fix exit code.
+
+Shell commands that:
+- exit with `0` when they succeed
+- exit with `1` when the fix skipped running
+- exit with anything else otherwise
--- core/execute.go
+++ core/execute.go
@@ -24,6 +24,12 @@ type setupConfig struct {
 }
 
 type stepConfig struct {
+	Key     string      `yaml:"key"`
+	Command string      `yaml:"command"`
+	Fixes   []fixConfig `yaml:"fixes,omitempty"`
+}
+
+type fixConfig struct {
 	Key     string `yaml:"key"`
 	Command string `yaml:"command"`
 }
@@ -56,10 +62,59 @@ type executor struct {
 func (e *executor) Execute() error {
 	for _, s := range e.config.Steps {
 		err := executeStep(e.projectPath, &s)
+
+		// Step succeeded -> move onto next step
+		if err == nil {
+			continue
+		}
+
+		// No fixes -> exit with failure
+		if len(s.Fixes) == 0 {
+			return err
+		}
+
+		// Try some fixes
+		fmt.Printf("Step '%s' failed to run, trying fixes...\n", s.Key)
+		for _, f := range s.Fixes {
+			executeFix(e.projectPath, &f)
+		}
+
+		// Try the step again
+		err = executeStep(e.projectPath, &s)
+
+		// Fixes didn't work -> exit with failure
 		if err != nil {
 			return err
 		}
 	}
+	return nil
+}
+
+func executeFix(projectPath string, fix *fixConfig) error {
+	cmd := exec.Command("bash", "-c", fix.Command)
+	cmd.Env = os.Environ()
+	cmd.Dir = projectPath
+
+	err := cmd.Run()
+
+	// Some errors aren't failures
+	exitCode := cmd.ProcessState.ExitCode()
+	if exitCode == 1 {
+		fmt.Printf("Fix '%s' was skipped\n", fix.Key)
+		return nil
+	}
+	if exitCode != 0 {
+		fmt.Printf("[Warning] Fix '%s' exited with code %d\n", fix.Key, exitCode)
+		return nil
+	}
+
+	// Other falure with executing command
+	if err != nil {
+		return fmt.Errorf("Fix '%s' failed to run: %w", fix.Key, err)
+	}
+
+	fmt.Printf("Fix '%s' ran successfully\n", fix.Key)
+
 	return nil
 }
 
@@ -80,7 +135,6 @@ func executeStep(projectPath string, step *stepConfig) error {
 	fmt.Printf("Step '%s' ran successfully\n", step.Key)
 
 	return nil
-
 }
 
 func NewExecutor(projectPath string) (Executor, error) {
